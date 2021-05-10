eb571ff5c0b8977f7126dc9ca182a77a573b32a4
gopi <gopigaurav9@gmail.com>
2021-05-10 23:06:14 +0530

    Updates

---
+++ precache-manifest.46ea3c1101be80f2c05baa5f4c37ceb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07579040393a14e9d722b9dbfa6afeb",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "96aaa97cfe49343b7866",
+    "url": "/amazon-clone/static/css/2.69507427.chunk.css"
+  },
+  {
+    "revision": "0eb396dba7de592fd691",
+    "url": "/amazon-clone/static/css/main.3e57dc00.chunk.css"
+  },
+  {
+    "revision": "96aaa97cfe49343b7866",
+    "url": "/amazon-clone/static/js/2.5687d29a.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/amazon-clone/static/js/2.5687d29a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb396dba7de592fd691",
+    "url": "/amazon-clone/static/js/main.18baf96d.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
